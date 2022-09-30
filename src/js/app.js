a40780a53bbcd1a5339ebe1d2ca38590bf86b061
Хабибуллин Тимур Хаматнурович <KHabibullin.T@domparketa.corp>
2022-09-30 19:48:07 +0500

    add location

--- src/js/app.js
+++ src/js/app.js
@@ -4,31 +4,63 @@ flsFunctions.isWebp();
 
 
 
+document.addEventListener('DOMContentLoaded', () => {
+    const apiKey = '9c86ea28cf45230394a138f2e16b445c',
+        weatherTemp = document.querySelector('.weather'),
+        search = document.querySelector('.search'),
+        form = document.querySelector('.form'),
+        submit = document.querySelector('.submit');
 
-const apiKey = '9c86ea28cf45230394a138f2e16b445c',
-    weather = document.querySelector('.weather'),
-    search = document.querySelector('.search'),
-    submit = document.querySelector('.submit');
+    let city = 'Тюмень';
 
-let city = 'Tyumen';
-
-search.addEventListener('keydown', (e) => {
-    if (e.which === 13 || e.keyCode === 13 || e.key === 'Enter') {
+    form.addEventListener('submit', async(e) => {
         e.preventDefault();
         city = search.value;
-        getWeather();
+        await loadWeather();
+        form.reset();
+    });
+
+    async function getWeather() {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&appid=${apiKey}`);
+        const data = await response.json();
+        return data;
     }
+
+    async function getCoordsWeather() {
+        navigator.geolocation.getCurrentPosition(async(pos) => {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?&lang=ru&lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=${apiKey}`);
+            const data = await response.json();
+            console.log(data)
+            return data;
+        })
+    }
+
+    getCoordsWeather()
+
+
+    // Возвращаем объект из промиса
+
+    async function loadWeather() {
+        const weather = await getWeather();
+
+        let dateSunrise = new Date(weather.sys.sunrise * 1000),
+            dateSunset = new Date(weather.sys.sunset * 1000);
+
+        let dateSunriseTime = `${dateSunrise.getHours() + 3}:${dateSunrise.getMinutes()}:${dateSunrise.getSeconds()}`,
+            dateSunsetTime = `${dateSunset.getHours() + 3}:${dateSunset.getMinutes()}:${dateSunset.getSeconds()}`;
+
+
+        weatherTemp.textContent = `
+        ${Math.round(weather.main.temp - 273.15)} градусов в ${city},
+        Ощущается, как ${Math.round(weather.main.feels_like - 273.15)},
+        ${weather.weather[0].description},
+        ветер: ${weather.wind.speed},
+        Восход: ${dateSunriseTime},
+        Закат: ${dateSunsetTime}
+        `;
+    }
+
+
+
+    loadWeather();
 });
-
-
-function getWeather() {
-
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            weather.textContent = `${Math.round(data.main.temp - 273.15)} - градусов в ${city}`;
-        });
-}
-
-getWeather();
